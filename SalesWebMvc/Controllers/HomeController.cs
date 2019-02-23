d8c4b27a1f582eb0a08ca573ad1bae038710e39d
Anderson de Oliveira Lima <anderson.nlima@tivit.com>
2019-02-23 18:37:41 -0300

    Changing Theme

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Fale Comigo!";
 
             return View();
         }
